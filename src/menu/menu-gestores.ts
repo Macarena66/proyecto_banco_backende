0a505c4a654f9b4527fae15f5f4d5ed2623df932
CNA06P009\Tarde <macarenachaves@gmail.com>
2022-07-11 16:24:26 +0200

    refactorizacion de la validacion y el objeto wrapper

--- src/menu/menu-gestores.ts
+++ src/menu/menu-gestores.ts
@@ -2,45 +2,35 @@ import { Configuracion } from '../modelos/configuracion';
 import { BancoArchivos } from '../almacenamiento/banco-archivos';
 import validator from 'validator';
 import { BancoGestores } from '../opciones/banco-gestores';
+import { Wrapper } from '../modelos/wrapper';
 
-export async function mostrarMenuGestores (
-    conf: Configuracion,
-    bancoArchivos: BancoArchivos,
-    rlp) {
+export async function mostrarMenuGestores(w: Wrapper) {
 
-        let opcion: string;
-        const bancoGestores = new BancoGestores(conf, bancoArchivos, rlp)
+  let opcion: string;
+  const bancoGestores = new BancoGestores(w);
 
+  do {
 
-        do {
+    console.clear();
+    console.log('MENÚ GESTORES');
+    console.log('-------------');
+    console.log('1. Insertar gestor');
+    console.log('2. Insertar gestores masivamente');
+    console.log('3. Mostrar gestores');
+    console.log('4. Mostrar gestor por identificador');
+    console.log('5. Modificar gestor');
+    console.log('6. Eliminar gestor por identificador');
+    console.log('7. Atrás');
+    
+    opcion = await w.rlp.questionAsync('¿Qué opción deseas?\n');
 
-            console.clear();
-            console.log('MENÚ GESTORES');
-            console.log('-------------');
-            console.log('1. Insertar gestor');
-            console.log('2. Insertar gestores masivamente');
-            console.log('3. Mostrar gestores');
-            console.log('4. Mostrar gestor por identificador');
-            console.log('5. Modificar gestor');
-            console.log('6. Eliminar gestor por identificador');
-            console.log('7. Atrás');
-            
-            opcion = await rlp.questionAsync('¿Qué opción deseas?\n');
+    console.clear();
 
-            console.clear();
-
-            // Opción 1 --> Insertar gestor 
-            if(opcion === '1') {
-              await bancoGestores.insertarGestor(); 
-              await rlp.questionAsync('');       
-            }
-      
-            
-
-
-            //validator.isEmail('cursosatb@gmail.com')
-      
-          } while(opcion !== '7')
-
+    // Opción 1 --> Insertar gestor 
+    if(opcion === '1') {
+      await bancoGestores.insertarGestor();        
+      await w.rlp.questionAsync('');
     }
 
+  } while(opcion !== '7')
+}
--- src/menu/menu-principal.ts
+++ src/menu/menu-principal.ts
@@ -1,45 +1,38 @@
+import readline from 'readline-promise';
+
 import { Configuracion } from '../modelos/configuracion';
 import { BancoArchivos } from '../almacenamiento/banco-archivos';
-import readline from 'readline-promise';
 import { mostrarMenuGestores } from './menu-gestores';
+import { Wrapper } from '../modelos/wrapper';
 
 // export porque la función se utiliza fuera de este archivo
 // async porque dentro se utiliza await
-export async function mostrarMenuPrincipal(
-    conf: Configuracion,
-    bancoArchivos: BancoArchivos){
+export async function mostrarMenuPrincipal(w: Wrapper) {
 
-    //configuracion de readline-promise
-    const rlp = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-        terminal: false,
-      });
+  let opcion: string;
+
+  do {
+
+    console.clear();
+    console.log('BANCO');
+    console.log('-------------');
+    console.log('1. Gestores');
+    console.log('2. Clientes');
+    console.log('3. Mensajes');
+    console.log('4. Transferencias');
+    console.log('5. Otros');
+    console.log('6. Login');
+    console.log('0. Salir');
     
-      let opcion: string;
-
-      do{
-        console.clear();
-        console.log('BANCO');
-        console.log('1.Gestores');
-        console.log('2.Clientes');
-        console.log('3. Mensajes');
-        console.log('4. Transferencias');
-        console.log('5.Otros');
-        console.log('6. Login');
-        console.log('0. Salir');
-
-        opcion = await rlp.questionAsync('¿Que opción deseas?\n');
-
-        if(opcion === '1'){
-            await mostrarMenuGestores(conf, bancoArchivos, rlp);
-        }
-        
-        
-      }while(opcion !== '0');
-
-      //esta es la última línea del programa
-      rlp.close();
-
+    opcion = await w.rlp.questionAsync('¿Qué opción deseas?\n');
+    console.log(opcion);
+    
+    if(opcion === '1') {
+      await mostrarMenuGestores(w);
     }
 
+  } while(opcion !== '0');
+
+  // esta es la última línea del programa
+  w.rlp.close();
+}
