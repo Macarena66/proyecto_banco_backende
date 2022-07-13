ba36808e82f1d4a45822eba8183e74319597e7e1
CNA06P009\Tarde <macarenachaves@gmail.com>
2022-07-13 20:26:10 +0200

    validacion de archivo de configuracion y clase banco database

---
+++ src/almacenamiento/banco-database.ts
@@ -0,0 +1,32 @@
+import { Configuracion } from '../modelos/configuracion';
+import { Collection, MongoClient } from 'mongodb';
+import { Gestor } from '../modelos/gestor';
+
+export class BancoDatabase{
+    //atributos
+    private conf: Configuracion;
+    private cGestores: Collection<Gestor>
+
+    constructor(conf: Configuracion){
+        this.conf = conf;
+    }
+
+    async conectar(){
+        const uri = `mongodb://${this.conf.databaseHost}:${this.conf.databasePuerto}`
+        const client = new MongoClient(uri);
+
+        try {
+            await client.connect();
+            const db = client.db(this.conf.databaseNombre);
+            const cGestores = db.collection('gestores');
+          }
+          catch {
+            console.log('Error conectando a la base de datos');      
+          }
+          finally {//liberacion de recursos
+            await client.close();
+      
+          }
+        }
+      }
+
